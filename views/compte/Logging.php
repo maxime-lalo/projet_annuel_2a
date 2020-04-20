8d716bfde8d1ca7c959a2664293f8ad977b53363
Léo <Daviere-leo@hotmail.fr>
2020-04-20 19:03:41 +0200

    Adding Session Coockie with check

--- views/compte/Logging.php
+++ views/compte/Logging.php
@@ -9,8 +9,12 @@ if(isset($_POST['mail']) && isset($_POST['password'])){
     $authService = new AuthService($manager);
     $user = $authService->log($_POST['mail'],$_POST['password']);
     if(isset($user)){
-        var_dump($authService->getUserFromId($user));
+        if(isset($_POST['check']))
+            setcookie('user_id', $user, time()+2592000);
+        else
+        setcookie('user_id', $user);
     }
+    else echo "user introuvable";
 
 
 
--- views/compte/connexion.php
+++ views/compte/connexion.php
@@ -13,8 +13,8 @@
         <input required type="password" class="form-control" name="password">
     </div>
     <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <input type="checkbox" class="form-check-input" name="check">
+        <label class="form-check-label" for="check">Check me out</label>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
     </div>
