72a08e4a905e4b57151d340d79d74b4bbf9b3385
Léo <Daviere-leo@hotmail.fr>
2020-05-13 20:10:11 +0200

    Améliorations gestion

--- views/compte/gestionTruck.php
+++ views/compte/gestionTruck.php
@@ -1,7 +1,14 @@
 <hr>
 <br>
 <div class="container-fluid txt-container">
-    <table id="truckList" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+        <?php
+        require_once __DIR__ . "/../../repositories/FoodTruckRepository.php";
+
+        $truckService = new FoodTruckRepository();
+
+        if(!isset($_GET['id'])){
+        $trucks = $truckService->getAll();?>
+        <table id="truckList" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
         <thead>
         <tr>
             <th class="th-sm">Id</th>
@@ -10,14 +17,8 @@
             <th class="th-sm">Mileage</th>
         </tr>
         </thead>
-        <tbody>
-        <?php
-        require_once __DIR__ . "/../../repositories/FoodTruckRepository.php";
-
-        $truckService = new FoodTruckRepository();
-
-        $trucks = $truckService->getAll();
-
+        <tbody>    
+        <?php 
         foreach ($trucks
 
         as $truck){
@@ -26,16 +27,47 @@
         <tr>
             <td><?= $truck->getId(); ?></td>
 
-            <td><?= $truck->getDateRegister() ?></td>
+            <td><a href="/<?= LANG; ?>/compte/gestionTruck?id=<?=$truck->getId(); ?>"><?= $truck->getDateRegister() ?></a></td>
 
             <td><?= $truck->getDateCheck() ?></td>
 
             <td><?= $truck->getMileage() ?></td>
 <?php
-} ?>
+} ?></tbody>
+</table> <?php
+        }
+        else{
+          $truck = $truckService->getOneById($_GET['id']);
+          ?>
+          <div class="tab-pane" id="camions">
+              <h4>Gestion du Camion</h4>
+                        <hr>                        
+                            <div class="form-group">
+                            <div class="col-xs-6">
+                                    <label for="id"><h4>Id</h4></label>
+                                    <input required disabled type="text" class="form-control" name="id"
+                                           value="<?= $truck->getId(); ?>">
+                                </div>
+                                <div class="col-xs-6">
+                                    <label for="mileage"><h4>Mileage</h4></label>
+                                    <input required type="number" class="form-control" name="mileage"
+                                           value="<?= $truck->getMileage(); ?>">
+                                </div>
+                                <div class="col-xs-6">
+                                    <label for="register"><h4>Date Register</h4></label>
+                                    <input required  type="date" class="form-control" name="register"
+                                           value="<?= $truck->getDateRegister(); ?>">
+                                </div>
 
-</tbody>
-</table>
+                                <div class="col-xs-6">
+                                    <label for="lastcheck"><h4>Date last check</h4></label>
+                                    <input required  type="date" class="form-control" name="lastcheck"
+                                           value="<?= $truck->getDateCheck(); ?>">
+                                </div>
+                            </div>
+                            <?php
+        }
+?>
 </div>
 
 
--- views/compte/gestionWarehouse.php
+++ views/compte/gestionWarehouse.php
@@ -1,33 +1,36 @@
 <hr>
 <br>
 <div class="container-fluid txt-container">
-    <table id="warehouseList" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
-        <thead>
-        <tr>
-            <th class="th-sm">Id</th>
-            <th class="th-sm">Name</th>
-            <th class="th-sm">Street Name</th>
-            <th class="th-sm">Street Number</th>
-            <th class="th-sm">City</th>
-        </tr>
-        </thead>
-        <tbody>
+    
         <?php
         require_once __DIR__ . "/../../repositories/WarehouseRepository.php";
 
         $warehouseService = new WarehouseRepository();
 
-        $warehouses = $warehouseService->getAll();
-
+        if(!isset($_GET['id'])){
+            $warehouses = $warehouseService->getAll();
+            ?>
+            <table id="warehouseList" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+            <thead>
+            <tr>
+                <th class="th-sm">Id</th>
+                <th class="th-sm">Name</th>
+                <th class="th-sm">Street Name</th>
+                <th class="th-sm">Street Number</th>
+                <th class="th-sm">City</th>
+            </tr>
+            </thead>
+            <tbody>
+                <?php
         foreach ($warehouses
 
         as $warehouse){
         ?>
 
         <tr>
-            <td><?= $warehouse->getId(); ?></td>
+            <td><?= $warehouse->getId(); ?></a></td>
 
-            <td><?= $warehouse->getName() ?></td>
+            <td><a href="/<?= LANG; ?>/compte/gestionWarehouse?id=<?=$warehouse->getId(); ?>"><?= $warehouse->getName() ?></a></td>
 
             <td><?= $warehouse->getStreetName() ?></td>
 
@@ -37,9 +40,46 @@
 
 <?php
 } ?>
-
 </tbody>
 </table>
+<?php }
+else{
+    $warehouse = $warehouseService->getOneById($_GET['id']);
+    ?>
+    <div class="tab-pane" id="camions">
+        <h4>Gestion de l'entrepôt</h4>
+                  <hr>                        
+                      <div class="form-group">
+                      <div class="col-xs-6">
+                              <label for="id"><h4>Id</h4></label>
+                              <input required disabled type="text" class="form-control" name="id"
+                                     value="<?= $warehouse->getId(); ?>">
+                          </div>
+                          <div class="col-xs-6">
+                              <label for="name"><h4>Name</h4></label>
+                              <input required type="text" class="form-control" name="name"
+                                     value="<?= $warehouse->getName(); ?>">
+                          </div>
+                          <div class="col-xs-6">
+                              <label for="street"><h4>Street Name</h4></label>
+                              <input required  type="text" class="form-control" name="street"
+                                     value="<?= $warehouse->getStreetName(); ?>">
+                          </div>
+
+                          <div class="col-xs-6">
+                              <label for="number"><h4>Street Number</h4></label>
+                              <input required  type="number" class="form-control" name="number"
+                                     value="<?= $warehouse->getStreetNumber(); ?>">
+                          </div>
+                          <div class="col-xs-6">
+                              <label for="city"><h4>City</h4></label>
+                              <input required  type="text" class="form-control" name="city"
+                                     value="<?= $warehouse->getCity(); ?>">
+                          </div>
+                      </div>
+                      <?php
+  }
+?>
 </div>
 
 
