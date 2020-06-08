669abc1bfaa58d2aa15dc8751b7e0d84278ee088
Léo <Daviere-leo@hotmail.fr>
2020-06-08 18:47:04 +0200

    Adding Api user auth

--- api/user/get.php
+++ api/user/get.php
@@ -15,6 +15,21 @@ if (isset($_GET['id'])){
     }else{
         new JsonReturn(JsonReturn::ERROR,"User not found",404);
     }
-}else{
+}elseif(isset($_GET['email']) && isset($_GET['password'])){
+    $manager = new DatabaseManager();
+    $authService = new AuthService($manager);
+    $userID = $authService->log($_POST['mail'],$_POST['password']);
+    if ($userID != -1 && $userID != -2){
+        $user = $uRepository->getOneByEmail($_GET['email']);
+        if ($user != null){
+            new JsonReturn(JsonReturn::SUCCESS,"User found",200,$user);
+        }else{
+            new JsonReturn(JsonReturn::ERROR,"User not found",404);
+        }
+    }else{
+        new JsonReturn(JsonReturn::ERROR,"Wrong email or password",404);
+    }
+}
+else{
     new JsonReturn(JsonReturn::ERROR,"Missing id argument in get",400);
 }
