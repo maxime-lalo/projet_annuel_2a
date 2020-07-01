340db1a08a55a34a6db5436b3c4558df5fa02a4b
Maxime LALO <maxime.lalo.pro@gmail.com>
2020-07-01 13:37:40 +0200

    Removing comment on header API

--- api/header.php
+++ api/header.php
@@ -1,5 +1,5 @@
 <?php
-//header("Content-Type: application/json");
+header("Content-Type: application/json");
 require_once __DIR__ . "/Parameters.php";
 require_once __DIR__ . "/JsonReturn.php";
 $p = new Parameters();
