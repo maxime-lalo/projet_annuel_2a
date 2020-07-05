1cedd42620228d91f7b5acc1c77ac31a726ea1c1
Peter <peterbalivet@gmail.com>
2020-07-05 18:27:04 +0200

    Franchisee can now change FoodTruck location

--- views/franchisee/truck.php
+++ views/franchisee/truck.php
@@ -65,6 +65,30 @@ if (isset($_GET['setBreakdown'])){
             </tr>
             </tbody>
         </table>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th><?= translate("Ville");?></th>
+                <th><?= translate("Code postale");?></th>
+                <th><?= translate("N°");?></th>
+                <th><?= translate("Rue");?></th>
+                <th><?= translate("Actions");?></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td id="inputCity"><?= $truck->getCity();?></td>
+                <td id="inputZipCode"><?= $truck->getZipcode();?></td>
+                <td id="inputStreetNumber"><?= $truck->getStreetNumber();?></td>
+                <td id="inputStreetName"><?= $truck->getStreetName();?></td>
+                <td id="actions<?= $truck->getId();?>">
+                    <button class="btn btn-primary" title="<?= translate("Éditer");?>" data-toggle="tooltip" onclick="editTruck(<?= $truck->getId();?>)">
+                        <i class="fas fa-edit"></i>
+                    </button>    
+                </td>
+            </tr>
+            </tbody>
+        </table>
         <?php
         if (!$isOnBreakdown){
             ?>
@@ -78,3 +102,94 @@ if (isset($_GET['setBreakdown'])){
     }
     ?>
 </div>
+
+<script>
+    function editTruck(truck) {
+        var city = $('#inputCity');
+        var zipcode = $('#inputZipCode');
+        var streetNumber = $('#inputStreetNumber');
+        var streetName = $('#inputStreetName');
+
+        var cityVal = city.html();
+        var zipcodeVal = zipcode.html();
+        var streetNumerVal = streetNumber.html();
+        var streetNameVal = streetName.html();
+        city.html("<input type='text' class='form-control' value='" + cityVal + "'>");
+        zipcode.html("<input type='text' class='form-control' value='" + zipcodeVal + "'>");
+        streetNumber.html("<input type='number' class='form-control' value='" + streetNumerVal + "'>");
+        streetName.html("<input type='text' class='form-control' value='" + streetNameVal + "'>");
+
+        var actionsRow = $('#actions' + truck);
+        var btn =   '<button class="btn btn-success" data-toggle="tooltip" title="<?= translate("Valider");?>" onclick="submitEdit(' + truck + ')">' +
+                        '<i class="fa fa-check"></i>' +
+                    '</button>\n' +
+                    '<button class="btn btn-danger" data-toggle="tooltip" title="<?= translate("Annuler");?>" onclick="cancelEdit(\'' + cityVal + '\',\'' + zipcodeVal + '\',\'' + streetNumerVal + '\',\'' + streetNameVal + '\',' + truck +')">' +
+                        '<i class="fas fa-times"></i>' +
+                    '</button>';
+        actionsRow.html(btn);
+    }
+
+    function submitEdit(truck){
+        var city = $('#inputCity');
+        var zipcode = $('#inputZipCode');
+        var streetNumber = $('#inputStreetNumber');
+        var streetName = $('#inputStreetName');
+
+        var Truck = {id: truck, city: city.find('input').val(), zipcode: zipcode.find('input').val(), street_number: streetNumber.find('input').val(), street_name: streetName.find('input').val()}
+
+        $.ajax({
+            url : '/api/truck',
+            type: 'PUT',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(Truck)
+        }).done(function(data){
+            if(data.status === "success"){
+                city.html(Truck.city);
+                zipcode.html(Truck.zipcode);
+                streetNumber.html(Truck.street_number);
+                streetName.html(Truck.street_name);
+
+                var actionsRow = $('#actions' + truck);
+                actionsRow.html('<button class="btn btn-primary" title="<?= translate("Éditer");?>" data-toggle="tooltip" onclick="editTruck(' + truck + ')">' +
+                    '<i class="fas fa-edit"></i>' +
+                    '</button>\n'
+                );
+                Swal.fire({
+                    title: '<?= translate("Succès");?>',
+                    text: '<?= translate("Le camion a bien été mis à jour");?>',
+                    icon: "success"
+                });
+                $('.tooltip').remove();
+                $('[data-toggle="tooltip"]').tooltip();
+
+            }else{
+                Swal.fire({
+                    title: '<?= translate("Erreur");?>',
+                    text: '<?= translate("Erreur lors de la mise à jour du camion, les valeurs ne sont pas valables ou n\'ont pas changé");?>',
+                    icon: "error"
+                });
+            }
+        });
+    }
+
+    function cancelEdit(city, zipcode, streetNumber, streetName, truck){
+        var cityTd = $('#inputCity');
+        var zipcodeTd = $('#inputZipCode');
+        var streetNumberTd = $('#inputStreetNumber');
+        var streetNameTd = $('#inputStreetName');
+
+        cityTd.html(city);
+        zipcodeTd.html(zipcode);
+        streetNumberTd.html(streetNumber);
+        streetNameTd.html(streetName);
+
+        var actionsRow = $('#actions' + truck);
+        actionsRow.html('<button class="btn btn-primary" title="<?= translate("Éditer");?>" data-toggle="tooltip" onclick="editTruck(' + truck + ')">' +
+            '<i class="fas fa-edit"></i>' +
+            '</button>\n' 
+        );
+
+        $('[data-toggle="tooltip"]').tooltip();
+    }
+</script>
