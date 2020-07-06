e91cbf651cda0441cd999f9c358ea5d48ad7cd82
Maxime Lalo <maxime.lalo.pro@gmail.com>
2020-07-06 10:23:33 +0200

    mailer working on index

--- public/index.php
+++ public/index.php
@@ -7,23 +7,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 
 // Create the Transport
-$transport = (new Swift_SmtpTransport('tls://smtp.gmail.com', 587))
-    ->setUsername('maxime.lalo.pro@gmail.com')
-    ->setPassword('ac5aabba1e&&')
+$transport = (new Swift_SmtpTransport('ssl://smtp.gmail.com', 465))
+    ->setUsername('eplp.lloret@gmail.com')
+    ->setPassword('ac5aabba1e')
 ;
 
 // Create the Mailer using your created Transport
 $mailer = new Swift_Mailer($transport);
-
-// Create a message
-$message = (new Swift_Message('Wonderful Subject'))
-    ->setFrom(['john@doe.com' => 'John Doe'])
-    ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
-    ->setBody('Here is the message itself')
-;
-
-// Send the message
-$result = $mailer->send($message);
 
 $url = $_SERVER['REQUEST_URI'];
 $url = substr($url, 1,strlen($url));
