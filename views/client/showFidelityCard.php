ca57c687e510fbf73a0817a7e486e9e8e026e9a3
Maxime LALO <maxime.lalo.pro@gmail.com>
2020-07-09 15:14:36 +0200

    Fidelity Card updated

--- views/client/showFidelityCard.php
+++ views/client/showFidelityCard.php
@@ -2,4 +2,4 @@
 require_once __DIR__ . "/../../utils/qrcode/qrlib.php";
 require_once __DIR__ . "/../../repositories/UserRepository.php";
 
-QrCode::png($user->getId());
+QrCode::png("|id:".$user->getId()."-fst:".$user->getFirstName()."-lst:".$user->getLastName()."|");
